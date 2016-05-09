dcf8cb7818c7ac440d797c25d658caa8d9f40eb0
SusrithaMunukutla <saim@virtusa.com>
2016-05-09 14:23:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 git clone https://github.com/samhitha30/ionic-app.git /application
 cd /application
 unzip sensor.zip
+git clone https://github.com/SusrithaMunukutla/apk_file.git 
 cd sensor
 ionic build android
-cd ..
-git clone https://github.com/SusrithaMunukutla/apk_file.git /apk
-cp /application/sensor/platforms/android/build/outputs/apk/android-debug.apk /apk
+cd /application/apk_file
+cp /application/sensor/platforms/android/build/outputs/apk/android-debug.apk /application/apk_file
 git init
 git remote set-url origin https://SusrithaMunukutla:welcome123@github.com/SusrithaMunukutla/apk_file.git
 git config --global user.email "saim@virtusa.com"
@@ -14,7 +14,7 @@ git config --global user.name "SusrithaMunukutla"
 #git rm -f androi-debug.apk
 #git commit -m 'removed'
 #git push
-git add -u android-debug.apk
+git add .
 git commit -m 'added apk'
 git remote -v
 git push
